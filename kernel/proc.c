99ac3672f99bffb9f121e8571363d7b18ae7ec00
MaZhiming <mazhiming2@huawei.com>
2023-07-15 23:44:17 +0800

    [assignment3] add page for usyscall
    --------
    
    Signed-off-by: MaZhiming <mazhiming2@huawei.com>

--- kernel/proc.c
+++ kernel/proc.c
@@ -132,6 +132,16 @@ found:
     return 0;
   }
 
+  // Allocate a usyscall page
+  if((p->usyscall = (struct usyscall *)kalloc()) == 0){
+	kfree(p->trapframe);
+    freeproc(p);
+	release(&p->lock);
+	return 0;
+  }
+  // record the pid in the usyscall page
+  p->usyscall->pid = p->pid;
+
   // An empty user page table.
   p->pagetable = proc_pagetable(p);
   if(p->pagetable == 0){
@@ -158,6 +168,9 @@ freeproc(struct proc *p)
   if(p->trapframe)
     kfree((void*)p->trapframe);
   p->trapframe = 0;
+  if(p->usyscall)
+	kfree((void*)p->usyscall);
+  p->usyscall = 0;
   if(p->pagetable)
     proc_freepagetable(p->pagetable, p->sz);
   p->pagetable = 0;
@@ -172,7 +185,7 @@ freeproc(struct proc *p)
 }
 
 // Create a user page table for a given process, with no user memory,
-// but with trampoline and trapframe pages.
+// but with trampoline, trapframe and usyscall pages.
 pagetable_t
 proc_pagetable(struct proc *p)
 {
@@ -202,6 +215,15 @@ proc_pagetable(struct proc *p)
     return 0;
   }
 
+  // map the usycall below the trapframe page.
+  if(mappages(pagetable, USYSCALL, PGSIZE,
+			  (uint64)(p->usyscall), PTE_R | PTE_U) < 0){
+    uvmunmap(pagetable, TRAMPOLINE, 1, 0);
+	uvmunmap(pagetable, USYSCALL, 1, 0);
+	uvmfree(pagetable, 0);
+	return 0;
+  }
+
   return pagetable;
 }
 
@@ -212,6 +234,7 @@ proc_freepagetable(pagetable_t pagetable, uint64 sz)
 {
   uvmunmap(pagetable, TRAMPOLINE, 1, 0);
   uvmunmap(pagetable, TRAPFRAME, 1, 0);
+  uvmunmap(pagetable, USYSCALL, 1, 0);
   uvmfree(pagetable, sz);
 }
 
